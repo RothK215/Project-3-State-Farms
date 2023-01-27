cbb52c43bbf2778a94960480e83c909d6fa26504
Rothana <112769542+RothK215@users.noreply.github.com>
2023-01-27 12:06:10 -0500

    modified

---
+++ pie_plots2.js
@@ -0,0 +1,67 @@
+function buildMetadata(states) {
+    d3.json("https://raw.githubusercontent.com/RothK215/Project-3-State-Farms/main/json/pie_data_url.json").then((data) => {
+        let metadata = data.metadata;
+
+        let resultArray = metadata.filter(sampleObj => sampleObj.id == states);
+        let result = resultArray[0];
+
+        let PANEL = d3.select("#state-metadata");
+        PANEL.html("");
+
+        for (key in result){
+            PANEL.append("h6").text(`${key.toUpperCase()}: ${result[key]}`);
+        };
+
+    });
+}
+
+function buildCharts(states) {
+    d3.json("https://raw.githubusercontent.com/RothK215/Project-3-State-Farms/main/json/pie_data_url.json").then((data) => {
+    let stateData = data.metadata
+    let resultsArray = stateData.filter(sampleObj => sampleObj.id == states);
+    let result = resultsArray[0];
+
+    let state_name = result.state;
+    let state_values = result.crop;
+
+
+    // Display the default plot
+    let pieData = [{
+        values: state_values,
+        labels: state_name,
+        type: "pie"
+    }];
+
+    let layout = {
+        height: 500,
+        width: 500,
+        plot_bgcolor: "rgba(0,0,0,0)",
+        paper_bgcolor: "rgba(0,0,0,0)"       
+    };
+    
+  
+  Plotly.newPlot("pie", pieData, layout);
+});
+}
+
+function init() {
+    let selector = d3.select("#selDataset");
+    d3.json("https://raw.githubusercontent.com/RothK215/Project-3-State-Farms/main/json/pie_data_url.json").then((data) => {
+    let stateNames = data.metadata.state;
+
+    for (let i = 0; i<54; i++) {
+        selector
+            .append("option")
+            .text(stateNames[i])
+            .property("value", stateNames[i])
+    };
+    let newState = stateNames.state[i];
+    buildCharts(newState);
+    });
+}
+
+function optionChanged(newState) {
+    buildCharts(newState);
+}
+
+init();
